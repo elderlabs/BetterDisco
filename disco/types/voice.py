9f63b0316c7cc42949aac8ced3d4c1bcf724226d
“elderlabs” <elderlabs@users.noreply.github.com>
2020-04-05 16:25:35 -0400

    Map missing VoiceState API bits

--- disco/types/voice.py
+++ disco/types/voice.py
@@ -1,15 +1,19 @@
+from disco.types import GuildMember
 from disco.types.base import SlottedModel, text, Field, snowflake, cached_property
 
 
 class VoiceState(SlottedModel):
-    session_id = Field(str)
     guild_id = Field(snowflake)
     channel_id = Field(snowflake)
     user_id = Field(snowflake)
+    member = Field(GuildMember)
+    session_id = Field(str)
     deaf = Field(bool)
     mute = Field(bool)
     self_deaf = Field(bool)
     self_mute = Field(bool)
+    self_stream = Field(bool)
+    self_video = Field(bool)
     suppress = Field(bool)
 
     @cached_property
