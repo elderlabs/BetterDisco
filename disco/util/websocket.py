d17d1dc18862ea15f699993d5ab011e201dd28a4
August <Hackerjef@users.noreply.github.com>
2024-02-22 03:23:02 -0500

    BUGFIX: Update websocket.py
    Fix bug on windows w/ timeouts

--- disco/util/websocket.py
+++ disco/util/websocket.py
@@ -14,7 +14,12 @@ class Websocket(LoggingClass, websocket.WebSocketApp):
     """
     def __init__(self, *args, **kwargs):
         LoggingClass.__init__(self)
-        websocket.setdefaulttimeout(5)
+
+        if platform.system() != "Windows":
+            websocket.setdefaulttimeout(5)
+        else:
+            self.log.warning("Running on windows may result in the websocket timing out, Due to a bug in websocket & timeouts")
+        
         websocket.WebSocketApp.__init__(self, *args, **kwargs)
 
         self.emitter = Emitter()
